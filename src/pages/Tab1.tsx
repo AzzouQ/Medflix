ba4df6e9c1b6b95e1ac9c1736a84c787d87a62c1
Nathan Quesseveur <nathan.quesseveur-julien@epitech.eu>
2021-04-18 14:42:57 +0200

    merge

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 
 const Tab1: React.FC = () => {
   return (
