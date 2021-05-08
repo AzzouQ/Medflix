c02fdf39093386eba7261bc6b4d796526a2c9ae1
Nathan Quesseveur <nathan.quesseveur-julien@epitech.eu>
2021-05-08 15:05:53 +0200

    propre

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,7 +3,7 @@ import { Typography, Row, Col } from 'antd';
 import { Player } from '@lottiefiles/react-lottie-player';
 import lottieAnim from '../assets/21474-medical-frontliners.json';
 
-import SignInForm from '../components/SigninForm';
+import SignInForm from '../components/SignInForm';
 import {
   IonContent,
   IonHeader,
