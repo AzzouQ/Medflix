b12fe61697e5872608c76771ff2a2101183586af
Nazim Klouche <nazim.klouche@epitech.eu>
2021-05-14 15:13:18 +0200

    edit mail and password done without fields verif

--- src/components/EditProfileForm.tsx
+++ src/components/EditProfileForm.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from 'antd';
+import firebase from 'firebase';
 import { Formik, FormikHelpers } from 'formik';
 import { Form, Input, SubmitButton } from 'formik-antd';
 import React from 'react';
@@ -6,12 +7,13 @@ import { useHistory } from 'react-router';
 import * as Yup from 'yup';
 import { t } from '../i18n';
 import LoadingModal from './LoadingModal';
+import { signOut } from '../service/firebase/auth';
 
 type EditProfileFormalues = {
   name: string;
   desc: string;
   email: string;
-  oldPassword: string;
+  currentPassword: string;
   password: string;
 };
 
@@ -25,14 +27,90 @@ const EditProfileForm: React.FC = () => {
       .email("L'email est invalide"),
   });
 
+  const reauthenticate = (
+    currentPassword: string
+  ): Promise<firebase.auth.UserCredential> | undefined => {
+    var user = firebase.auth().currentUser;
+    if (user && user.email) {
+      var cred = firebase.auth.EmailAuthProvider.credential(
+        user.email,
+        currentPassword
+      );
+      return user.reauthenticateWithCredential(cred);
+    } else return undefined;
+  };
+
+  const changeName = (name: string) => {
+    var user = firebase.auth().currentUser;
+    user && user.updateProfile({ displayName: name });
+  };
+
+  const changeDesc = (desc: string) => {
+    var user = firebase.auth().currentUser;
+    // user && user.updateDesc(desc);
+  };
+
+  const changePassword = async (
+    currentPassword: string,
+    newPassword: string
+  ) => {
+    const reAuth = reauthenticate(currentPassword);
+    if (reAuth) {
+      await reAuth
+        .then(() => {
+          var user = firebase.auth().currentUser;
+          user &&
+            user
+              .updatePassword(newPassword)
+              .then(() => {
+                console.log('Password updated!');
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  const changeEmail = async (currentPassword: string, newEmail: string) => {
+    const reAuth = reauthenticate(currentPassword);
+    if (reAuth) {
+      await reAuth
+        .then(() => {
+          var user = firebase.auth().currentUser;
+          user &&
+            user
+              .updateEmail(newEmail)
+              .then(() => {
+                console.log('Email updated!');
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   const formikSubmit = async (
-    { name, desc, email, oldPassword, password }: EditProfileFormalues,
+    { name, desc, email, currentPassword, password }: EditProfileFormalues,
     { setSubmitting, setFieldValue }: FormikHelpers<EditProfileFormalues>
   ) => {
     setFieldValue('password', '', false);
     try {
-      //   await signIn(email, password);
-      push('/profile');
+      if (name) changeName(name);
+      if (desc) changeDesc(desc);
+      if (email && currentPassword) await changeEmail(currentPassword, email);
+      if (currentPassword && password) {
+        await changePassword(currentPassword, password);
+      }
+      signOut();
+      push('/home');
     } catch (e) {
       console.log(e);
       // TODO Handle error
@@ -56,16 +134,17 @@ const EditProfileForm: React.FC = () => {
   const renderEditName = (): React.ReactElement | null => (
     <Row gutter={20}>
       <Col span={24}>
-        <Form.Item name={'name'} label={name} required={true}>
+        <Form.Item name={'name'} label={name}>
           <Input name={'name'} placeholder={namePlaceholder} />
         </Form.Item>
       </Col>
     </Row>
   );
+
   const renderEditDesc = (): React.ReactElement | null => (
     <Row gutter={20}>
       <Col span={24}>
-        <Form.Item name={'desc'} label={desc} required={true}>
+        <Form.Item name={'desc'} label={desc}>
           <Input name={'desc'} placeholder={descPlaceholder} />
         </Form.Item>
       </Col>
@@ -75,7 +154,7 @@ const EditProfileForm: React.FC = () => {
   const renderEditEmail = (): React.ReactElement | null => (
     <Row gutter={20}>
       <Col span={24}>
-        <Form.Item name={'email'} label={mail} required={true}>
+        <Form.Item name={'email'} label={mail}>
           <Input name={'email'} placeholder={mailPlaceholder} />
         </Form.Item>
       </Col>
@@ -86,9 +165,9 @@ const EditProfileForm: React.FC = () => {
     <>
       <Row gutter={20}>
         <Col span={24}>
-          <Form.Item name={'oldPassword'} label={oldPass} required={true}>
+          <Form.Item name={'currentPassword'} label={oldPass}>
             <Input.Password
-              name={'oldPassword'}
+              name={'currentPassword'}
               placeholder={oldPassPlaceholder}
               allowClear={true}
             />
@@ -97,7 +176,7 @@ const EditProfileForm: React.FC = () => {
       </Row>
       <Row gutter={20}>
         <Col span={24}>
-          <Form.Item name={'password'} label={newPass} required={true}>
+          <Form.Item name={'password'} label={newPass}>
             <Input.Password
               name={'password'}
               placeholder={newPassPlaceholder}
@@ -127,11 +206,11 @@ const EditProfileForm: React.FC = () => {
         name: '',
         desc: '',
         email: '',
-        oldPassword: '',
+        currentPassword: '',
         password: '',
       }}
       onSubmit={formikSubmit}
-      validationSchema={yupValidation}
+      // validationSchema={yupValidation}
     >
       {(formik) => (
         <Form name={'edit-profile'} size={'middle'} layout={'vertical'}>
